b46acbda35e93e02f1bfec910722957fb563e887
Halina2610 <vievamaxa@gmil.com>
2023-08-11 01:38:14 +0300

    lesson0

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Accordion } from './components/accordion/Accordion';
+import {Rating} from "./components/rating/Rating";
+
+/*function Hello() {
+    //debugger //останавливает программу и выполняет ее по шагам
+    alert('Hello IT-KAMASUTRA')
+}
+Hello();*/
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//    debugger //останавливает программу и выполняет ее по шагам
+    return (
+        <div>
+            <PageTitle title={"This is App component"}/>
+            <PageTitle title={"Users"}/>
+            <Accordion title={"List 1"} collapsed={true}/>
+            <Accordion title={"List 2"} collapsed={false}/>
+            <Rating value={0}/>
+            <Rating value={1}/>
+            <Rating value={2}/>
+            <Rating value={3}/>
+            <Rating value={4}/>
+            <Rating value={5}/>
+
+        </div>
+    );
+}
+
+type PageTitlePropsType = {
+    title: string
+}
+function PageTitle(props: PageTitlePropsType) {
+    return (
+        <h1>{props.title}</h1>
+    )
 }
 
 export default App;
