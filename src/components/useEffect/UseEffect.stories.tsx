f02686d74343bcfffb3b0a12ccf5a208342c1a9d
Halina2610 <vievamaxa@gmail.com>
2023-12-03 19:44:53 +0300

    25 useEffect

--- src/components/useEffect/UseEffect.stories.tsx
+++ src/components/useEffect/UseEffect.stories.tsx
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from "react";
+import {_extends} from "../../../storybook-static/sb-manager/chunk-QPL63VNK";
 
 export default {
     title: 'useEffect demo'
@@ -37,32 +38,106 @@ export const SimpleExample = () => {
     </>
 }
 
-export const SetTimeoutExample = () => {
-    const [counter, setCounter] = useState(1)
-    const [fake, setFake] = useState(1)
+export const SetTimeoutIntervalExample = () => {
+    const [counter, setCounter] = useState(1);
+    const [fake, setFake] = useState(1);
 
-    console.log('SetTimeoutExample');
+    console.log('SetTimeoutIntervalExample');
 
     useEffect(() => {
-        setTimeout(() => {
+        const timeoutId = setTimeout(() => {
             console.log('setTimeout');
-
-            document.title = counter.toString()
+            document.title = counter.toString();
         }, 1000);
-    }, [])
 
+        return () => {
+            clearTimeout(timeoutId);
+        };
+    }, [counter]);
 
     useEffect(() => {
-        setInterval(() => {
+        const intervalId = setInterval(() => {
             console.log('setInterval');
-            setFake((state) => state + 1)
+            setFake((state) => state + 1);
         }, 1000);
-    }, [])
 
+        return () => clearInterval(intervalId);
+    }, []);
+
+    return (
+        <>
+            <div>
+                counter: {counter}
+                <button onClick={() => setCounter((prevCounter) => prevCounter + 1)}>+</button>
+            </div>
+            <div>fake: {fake}</div>
+        </>
+    );
+};
+
+
+export const ResetEffectExample = () => {
+    const [counter, setCounter] = useState(1)
+
+    console.log('Component rendered')
+
+    useEffect(() => {
+        console.log("Effect occurred")
+        //при размантировании компаненты сбросим эффект:
+        return () => {
+            console.log('Reset Effect')
+        }
+    }, [counter]); //зависим от счетчика
+
+    return <div>
+        Hello, counter: {counter}
+        <button onClick={() => {
+            setCounter(counter + 1)
+        }}>
+            +
+        </button>
+    </div>
+}
+
+export const KeyTrackerExample = () => {
+    const [text, setText] = useState('')
+
+    console.log('Component rendered' + text)
+
+    useEffect(() => {
+        const handler = (e: KeyboardEvent) => {
+            console.log(e.key)
+            setText(text + e.key)
+        }
+
+        window.addEventListener('keypress', handler)
+        //при размантировании компаненты сбросим эффект:
+
+        return () => {
+            window.removeEventListener('keypress', handler)
+        }
+    }, [text]);
 
     return <>
-        counter: {counter}<button onClick={() => setCounter(counter + 1)}>+</button>
-        - fake: {fake}
+        Typed text: {text}
+    </>
+}
 
+export const SetTimeoutExample = () => {
+    const [text, setText] = useState('')
+
+    console.log('Component rendered' + text)
+
+    useEffect(() => {
+      const timeoutId =  setTimeout(() => {
+            setText('3 seconds passed')
+        }, 3000)
+
+        return () => clearTimeout(timeoutId)
+
+    }, [text]);
+
+    return <>
+        Typed text: {text}
     </>
 }
